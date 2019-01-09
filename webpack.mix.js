60bd135d7f75191a2ba4d58949a7447ec35691e5
devalfe <mdt.webmaster@gmail.com>
2019-01-09 14:52:18 -0500

    archivos iniciales

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,5 @@
 const mix = require('laravel-mix');
+const path = require('path');
 
 /*
  |--------------------------------------------------------------------------
@@ -10,6 +11,23 @@ const mix = require('laravel-mix');
  | file for the application as well as bundling up all the JS files.
  |
  */
+let themes = [
+    'resources/sass/themes/default-theme.scss',
+    'resources/sass/themes/lix-theme.scss'
+]
+themes.forEach((item) => {
+    mix.sass(item, 'public/css/themes')
+})
+
 
 mix.js('resources/js/app.js', 'public/js')
-   .sass('resources/sass/app.scss', 'public/css');
+    .sass('resources/sass/app.scss', 'public/css')
+    .js('resources/js/main.js', 'public/js')
+    .sass('resources/sass/main.scss', 'public/css')
+
+.sourceMaps()
+    .disableNotifications()
+
+if (mix.inProduction()) {
+    mix.version()
+}
